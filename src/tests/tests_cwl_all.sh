61adf18f274551f03b2518c844f7c91b339f30ad
Lmercadom <42242797+Lmercadom@users.noreply.github.com>
2019-06-21 09:47:38 -0400

    Update tests_cwl_all.sh

--- src/tests/tests_cwl_all.sh
+++ src/tests/tests_cwl_all.sh
@@ -1,5 +1,5 @@
 set -e
-CWL_NAME_LIST=`ls -1 src/cwl/*cwl | sed 's/\.cwl//g' | sed 's/cwl\////g'`
+CWL_NAME_LIST=`ls -1 src/cwl/*cwl | sed 's/\./cwl//g' | sed 's/src\/cwl\///g'`
 echo $CWL_NAME_LIST
 for CWL_NAME in $CWL_NAME_LIST; do
        source src/tests/tests_cwl.sh $CWL_NAME;
